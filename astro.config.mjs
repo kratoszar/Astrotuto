7444d3fd171460676c84056baf9b7ff819c56f4b
Alberto Rosario Zarco <albertozarco57@gmail.com>
2026-05-21 16:51:48 -0600

    configuracion js

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import preact from '@astrojs/preact';
+
 export default defineConfig({
   // Agrega tu URL de Netlify aquí
-  site: 'https://astrootuto.netlify.app', 
+  site: 'https://astrootuto.netlify.app',
+
+  integrations: [preact()],
 });
