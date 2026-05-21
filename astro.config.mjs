2c5776096fb524436b711eae3b7b027e508cf5c1
Alberto Rosario Zarco <albertozarco57@gmail.com>
2026-05-21 16:31:07 -0600

    Fix: agregar propiedad site para el RSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
-
-site: "https://astrootuto.netlify.app"
+export default defineConfig({
+  // Agrega tu URL de Netlify aquí
+  site: 'https://astrootuto.netlify.app', 
+});
